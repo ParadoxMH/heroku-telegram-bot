491fe5606bf9e6218764df515454a1fa485e134b
prdx <p@m.s>
2022-01-30 15:35:10 +0200

    upd

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",        
                             port=int(PORT),                       
                             url_path=TOKEN) 
-    updater.bot.setWebhook('https://your-app-name.herokuapp.com/' + TOKEN) 
+    updater.bot.setWebhook('https://gerokubot.herokuapp.com/' + TOKEN) 
     updater.idle()
 
 if __name__ == '__main__':
